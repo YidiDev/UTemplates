d1740badcc4212cecb9684ea3fd1607fb0c5dd72
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-10-16 18:52:07 -0400

    adding render

--- UTemplates/rendering.py
+++ UTemplates/rendering.py
@@ -2,12 +2,15 @@ import os.path
 from .general_base import GeneralBaseElement
 
 
-def save_to_file(html_content: str | GeneralBaseElement | list[str | GeneralBaseElement], file_path: any) -> None:
+def render(
+        html_content: str | GeneralBaseElement | list[str | GeneralBaseElement] | tuple[str | GeneralBaseElement]
+) -> str:
     if isinstance(html_content, (str, GeneralBaseElement)):
         html_content: list[str | GeneralBaseElement] = [html_content]
+    return "".join(str(html_element) for html_element in html_content)
 
-    html_str: str = "".join(str(html_element) for html_element in html_content)
 
+def save_to_file(html_str: str, file_path: any) -> None:
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
